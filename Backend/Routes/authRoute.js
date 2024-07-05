68acdd0b2e99ef2c11bb43deb59226b309329864
Vijay Krishan <vk856549@gmail.com>
2024-07-05 07:36:37 +0530

    added authorization

--- Backend/Routes/authRoute.js
+++ Backend/Routes/authRoute.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { login, verifyLogin } = require('../Controllers/authController')
+const { login, verifyLogin, logout } = require('../Controllers/authController')
 const router = express.Router()
 
 router.post('/login', login )
 router.get('/verify',verifyLogin)
+router.get('/logout',logout)
 
 module.exports = router
